655abcd257f6130ef74cab0426317b0dd18b24db
Mustapha7018 <adamsmustapha419@gmail.com>
2024-06-25 01:48:50 +0000

    add search functionality

--- any_jokes/lib/data/providers/jokes_api.dart
+++ any_jokes/lib/data/providers/jokes_api.dart
@@ -2,21 +2,31 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class JokesApi {
-  static const String baseUrl = 'https://v2.jokeapi.dev/';
+  static const String baseUrl = 'https://v2.jokeapi.dev/joke/Any';
 
-  Future<List<Joke>> fetchJokes() async {
-    final response = await http.get(Uri.parse('$baseUrl/jokes'));
-    if (response.statusCode == 200) {
-      final jsonData = json.decode(response.body);
-      if (jsonData['error'] == false) {
-        final jokesData = jsonData['jokes'];
-        return List<Joke>.from(jokesData.map((joke) => Joke.fromJson(joke)));
+  Future<List<Joke>> fetchJokes({String? category, String? search, int count = 5}) async {
+    List<Joke> jokes = [];
+    for (int i = 0; i < count; i++) {
+      final url = Uri.parse(
+          '$baseUrl?type=twopart${category != null ? '&category=$category' : ''}${search != null ? '&contains=$search' : ''}');
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        print(jsonData); // Debug: Print the response body
+
+        if (jsonData['error'] == false) {
+          if (jsonData['type'] == 'twopart') {
+            jokes.add(Joke.fromJson(jsonData));
+          }
+        } else {
+          throw Exception('Failed to fetch jokes');
+        }
       } else {
         throw Exception('Failed to fetch jokes');
       }
-    } else {
-      throw Exception('Failed to fetch jokes');
     }
+    return jokes;
   }
 }
 
--- any_jokes/lib/home_page.dart
+++ any_jokes/lib/home_page.dart
@@ -1,122 +1,228 @@
-import 'dart:convert';
-
-import 'package:any_jokes/data/models/jokes_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
+import 'data/providers/jokes_api.dart';
 
-class AnyJokes extends StatefulWidget {
+class AnyJokes extends StatelessWidget {
   const AnyJokes({super.key});
 
   @override
-  State<AnyJokes> createState() => _AnyJokesState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
 }
 
-class _AnyJokesState extends State<AnyJokes> {
-  Future<Welcome> getJoke() async {
-    final response = await http.get(Uri.parse('https://v2.jokeapi.dev/joke/Any'));
-    if (response.statusCode == 200) {
-    
-        return Welcome.fromJson(json.decode(response.body));
-    
-    }
-    throw Exception('Failed to fetch joke'); 
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _searchController = TextEditingController();
+  String _selectedCategory = 'Any';
+  Future<List<Joke>>? _jokesFuture;
+
+  void _searchJokes() {
+    setState(() {
+      _jokesFuture = JokesApi().fetchJokes(
+        category: _selectedCategory == 'Any' ? null : _selectedCategory,
+        search: _searchController.text,
+        count: 5,
+      );
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent, // Transparent status bar
-      statusBarIconBrightness: Brightness.light, // Light icons
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
     ));
 
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 90, 14, 151),
-                Color.fromARGB(209, 83, 21, 150)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-            ),
-            child: FutureBuilder<Welcome>(
-            future: getJoke(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-              } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-              } else {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 90, 14, 151),
+              Color.fromARGB(209, 83, 21, 150),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
                 children: [
-                Column(
-                  children: [
                   const Text(
                     'Any Jokes',
                     style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 40,
+                      color: Colors.white,
+                      fontSize: 40,
                     ),
                   ),
                   const SizedBox(height: 20),
                   const Text(
-                    'Press the button below to get a joke',
+                    'Category',
                     style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  DropdownButton<String>(
+                    value: _selectedCategory,
+                    items: <String>[
+                      'Any',
+                      'Programming',
+                      'Misc',
+                      'Dark',
+                      'Pun',
+                      'Spooky',
+                      'Christmas'
+                    ].map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(
+                          value,
+                          style: const TextStyle(
+                              color: Colors.white), 
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (newValue) {
+                      setState(() {
+                        _selectedCategory = newValue!;
+                      });
+                    },
+                    dropdownColor: Colors.grey[800],
+                    iconEnabledColor:
+                        Colors.white, 
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16),
+                  ),
+                  const SizedBox(height: 20),
+                  TextField(
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                      hintText: 'Search for jokes...',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 15.0, horizontal: 20.0),
                     ),
                   ),
                   const SizedBox(height: 20),
-                  Card(
-                    color: Colors.white,
-                    margin: const EdgeInsets.all(16),
-                    child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Text(
-                      snapshot.data?.joke ?? '',
-                      style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.blue,
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 30,
+                      ),
+                      minimumSize: const Size.fromHeight(50),
+                    ),
+                    onPressed: _searchJokes,
+                    child: const Text(
+                      'Search Jokes',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
                       ),
                     ),
-                    ),
                   ),
-                  ],
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: Colors.blue,
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 15,
-                    horizontal: 30,
-                  ),
-                  ),
-                  onPressed: getJoke,
-                  child: const Text(
-                  'Get Joke',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                  ),
-                ),
                 ],
-              );
-              }
-            },
+              ),
             ),
-          ),
-          ),
-        );
-        }
-      }
+            Expanded(
+              child: FutureBuilder<List<Joke>>(
+                future: _jokesFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text(
+                        'Error: ${snapshot.error}',
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    );
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return Center(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        child: const Card(
+                          color: Colors.white,
+                          margin: EdgeInsets.all(16),
+                          child: Padding(
+                            padding: EdgeInsets.all(16),
+                            child: Text(
+                              'No jokes to show..Try browsing for jokes',
+                              style:
+                                  TextStyle(color: Colors.red, fontSize: 16),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  } else {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        final joke = snapshot.data![index];
+                        return Card(
+                          color: Colors.white,
+                          margin: const EdgeInsets.all(16),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  joke.setup,
+                                  style: const TextStyle(
+                                      color: Colors.black, fontSize: 18),
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  joke.punchline,
+                                  style: const TextStyle(
+                                      color: Colors.deepPurple,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
